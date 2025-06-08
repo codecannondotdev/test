2393c7df1513e1859e9c6c7388305520830dfa3f
codecannon Generator <generator@codecannon.dev>
2025-06-08 19:09:44 +0000

    feat: Generate modules

---
+++ api/app/Http/Controllers/CustomerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\UpdateCustomerRequest;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    private $crudControllerHelper;
+
+    public function __construct()
+    {
+        $this->crudControllerHelper = new CRUDControllerHelper(new Customer);
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function list(Request $request)
+    {
+        return $this->crudControllerHelper->list($request);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Customer $entity)
+    {
+        return $this->crudControllerHelper->show($entity);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCustomerRequest $request)
+    {
+        return $this->crudControllerHelper->store($request);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCustomerRequest $request, Customer $entity)
+    {
+        return $this->crudControllerHelper->update($request, $entity);
+    }
+
+    /**
+     * Update the specified resource relations in storage.
+     */
+    public function updateRelation(Request $request, Customer $entity, string $relation)
+    {
+        return $this->crudControllerHelper->updateRelation($request, $entity, $relation);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Customer $entity)
+    {
+        return $this->crudControllerHelper->destroy($entity);
+    }
+}
---
+++ api/app/Http/Requests/StoreSalesOrderRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreSalesOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'order_number' => 'required',
+            'order_date' => 'required',
+            'quantity' => 'required',
+            'unit_price' => 'required',
+            'total_amount' => 'required',
+            'car_model_id' => 'nullable',
+            'customer_id' => 'nullable',
+            'status' => 'required',
+        ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @return void
+     *
+     * @throws \Illuminate\Http\Exceptions\HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json(
+                [
+                    'message' => 'The given data was invalid.',
+                    'errors' => $validator->errors(),
+                ],
+                422
+            )
+        );
+    }
+}
