9b3b69607a717e236c3c8f33935949f74dcd69ab
codecannon Generator <generator@codecannon.dev>
2025-06-08 18:39:25 +0000

    feat: Initial commit

---
+++ api/app/Notifications/CustomVerifyEmail.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\VerifyEmail as BaseVerifyEmail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\URL;
+
+class CustomVerifyEmail extends BaseVerifyEmail
+{
+    use Queueable;
+
+    /**
+     * Get the verification URL for the given notifiable.
+     *
+     * @param  mixed  $notifiable
+     * @return string
+     */
+    protected function verificationUrl($notifiable)
+    {
+        // Generate the temporary signed URL
+        $temporarySignedUrl = URL::temporarySignedRoute(
+            'verification.verify',
+            Carbon::now()->addMinutes(config('auth.verification.expire', 60)),
+            ['id' => $notifiable->getKey(), 'hash' => sha1($notifiable->getEmailForVerification())]
+        );
+
+        // Get the configured app URLs
+        $appUrl = Config::get('app.url', 'http://localhost');
+        $uiUrl = Config::get('app.ui_url', 'http://localhost');
+
+        // Replace the API URL with the UI URL
+        return str_replace($appUrl, $uiUrl, $temporarySignedUrl);
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $verificationUrl = $this->verificationUrl($notifiable);
+
+        return (new MailMessage)
+            ->subject('Verify Email Address')
+            ->line('Please click the button below to verify your email address.')
+            ->action('Verify Email Address', $verificationUrl)
+            ->line('If you did not create an account, no further action is required.');
+    }
+}
---
+++ ui/src/views/NotFound.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="not-found">
+		<h1>404</h1>
+		<p>Oops... Page not found.</p>
+		<Button
+			icon="fal fa-home"
+			label="Take me home"
+			severity="secondary"
+			variant="outlined"
+			@click="router.replace('/')" />
+	</div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import Button from 'primevue/button'
+
+const router = useRouter()
+</script>
+
+<style lang="scss" scoped>
+.not-found {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: 100%;
+
+	h1 {
+		font-size: 200px;
+		font-weight: bold;
+		margin: 0;
+		line-height: 1;
+	}
+
+	p {
+		font-size: 20px;
+		line-height: 1;
+		margin: 30px;
+	}
+}
+</style>
